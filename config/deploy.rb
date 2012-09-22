8cd158d48f70f15d804db5ed7aacf7088420d7ab
matt <matthewcopeland1@gmail.com>
2012-09-21 19:35:30 -0700

    fist commit

---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+require "rvm/capistrano"
+set :rvm_ruby_string, 'default'
+
+# Bundler
+
+require "bundler/capistrano"
+
+# General
+
+set :application, "capteste"
+set :user, "deployer"
+set :port, 30000
+set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :copy
+
+set :use_sudo, false
+
+# Git
+
+set :scm, :git
+set :repository,  "git@heroku.com:guitarspeed.git"
+set :branch, "master"
+
+# VPS
+
+role :web, "173.255.252.56"
+role :app, "173.255.252.56"
+role :db,  "173.255.252.56", :primary => true
+role :db,  "173.255.252.56"
+
+# Passenger
+
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
+
+require 'capistrano_recipes'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capteste::Application.config.secret_token = 'de6601e44b04ef0e784d07f32c377a3b511e3125fa3933a1b353bb4c68bc6b8b953ec1bc4ebd14e6b6d49b8102fe5027d94268d3189a47e180016ba526be0acb'
