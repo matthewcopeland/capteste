f688cfb1b6ea73bdaafbceba18271591d6704629
matt <matthewcopeland1@gmail.com>
2012-09-26 00:11:11 -0700

    maybe dumb luck or database.yml? hrmm..

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,4 @@
 require "bundler/capistrano"
-set :default_environment, {
-# 'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-'PATH' => "/usr/local/rvm/bin/rvm"
-}
-
-
 
 server "96.126.100.112", :web, :app, :db, primary: true
 
